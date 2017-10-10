4da47e0bc8dc370485701c28014c556cf540ae7f
JIANLANG CHAI <707750125@qq.com>
2017-10-10 22:39:25 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+shinyUI(fluidPage(
+        titlePanel("Body Mass Index (BMI) Calculator"),
+        sidebarLayout(sidebarPanel(
+                helpText("The body mass index (BMI) calculator. Please, input your mass and heihgt."),
+                sliderInput(
+                        inputId = "height",
+                        label = strong("Your height in cm:"),
+                        min = 100,
+                        max = 240,
+                        value = 185
+                ), #User height input slider
+                sliderInput(
+                        inputId = "mass",
+                        label = strong("Your weight in kg:"),
+                        min = 20,
+                        max = 140,
+                        value = 74
+                ), #User mass input slider
+                h5("The BMI is an attempt to quantify the amount of tissue mass (muscle, fat, and bone) in an individual, and then categorize that person as underweight, normal weight, overweight, or obese based on that value."),
+                h6("The app was created by Jamin Chai as a Course Project for Coursera Developing Data Products")
+        ),
+        
+        mainPanel(
+                uiOutput("user_data"), #Text with the user data
+                plotOutput("plot", height = "175px", width = "100%"), #Colorful plot of the user BMI
+                uiOutput("results") #Text results
+        ))
+))
